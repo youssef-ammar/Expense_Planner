24564dac1d4a3103235506fd2626d00c2029ddb0
youssef-ammar <youssef.ammar19@yahoo.com>
2022-11-15 11:47:19 +0100

    model button sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,84 @@
-import 'widgets/chart.dart';
-
+import 'package:app/widgets/inputArea.dart';
+import 'package:app/widgets/list_transaction.dart';
 import 'package:flutter/material.dart';
-import 'widgets/trans.dart';
-import 'widgets/inputArea.dart';
+
+import 'models/transaction.dart';
+import 'widgets/chart.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: "New shoes", amount: 60.5, date: DateTime.now()),
+    Transaction(id: "t2", title: "eating", amount: 70, date: DateTime.now()),
+  ];
+  void _addNewTransaction(String tit, double am) {
+    setState(() {
+      //print("done")
+      transactions.add(Transaction(
+          id: DateTime.now().toString(),
+          title: tit,
+          amount: am,
+          date: DateTime.now()));
+    });
+  }
 
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Flutter App'),
-            ),
-            body: Column(
-                // by default mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Chart(),
-                  Input(),
-                  Trans(),
+    return  MaterialApp(
+          home:   FabExample(_addNewTransaction,transactions),
+        );
+  }
 
-                ])));
+}
+class FabExample extends StatelessWidget {
+final Function addtx;
+final List<Transaction> transactions;
+
+ FabExample(this.addtx,this.transactions);
+
+  void _startAddNewTrans(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return  Input(addtx);
+        });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        actions: [
+          IconButton(
+              onPressed: () => _startAddNewTrans(context),
+              icon: Icon(Icons.add))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+// by default mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Chart(),
+              Trans(transactions),
+            ]),
+      ),
+      floatingActionButtonLocation:
+      FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _startAddNewTrans(context),
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
--- lib/widgets/inputArea.dart
+++ lib/widgets/inputArea.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
-class Input extends StatelessWidget {
 
-  final titleController= TextEditingController();
-  final amountController=TextEditingController();
+class Input extends StatefulWidget {
+ // const Input({Key? key}) : super(key: key);
+  final Function addTx;
+
+  Input(this.addTx);
+  @override
+  State<Input> createState() => _InputState();
+}
+
+class _InputState extends State<Input> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  void submitData() {
+    final entredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if ((entredTitle.isEmpty) || (enteredAmount <= 0)) {
+      return;
+    }
+    widget.addTx(entredTitle, enteredAmount);
+    Navigator.of(context).pop();
+  }
   @override
   Widget build(BuildContext context) {
-    return  Card(
+    return Card(
       child: Container(
         padding: EdgeInsets.all(10),
         child: Column(
@@ -14,18 +33,25 @@ class Input extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
             ),
-            TextButton(onPressed: (){
-            print(titleController.text);}, child: Text('Add Transaction', style:TextStyle(color:Colors.purple),),
+            TextButton(
+              onPressed: submitData,
+              child: Text(
+                'Add Transaction',
+                style: TextStyle(color: Colors.purple),
+              ),
             ),
-
           ],
         ),
       ),
     );
   }
 }
+
